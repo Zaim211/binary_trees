60e2a36bfbabcc73a059250884e9758eb5553641
Zaim211 <yousseftitzaim@gmail.com>
2023-11-01 13:04:08 -0700

    In-order traversal

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -6,12 +6,12 @@
  * @func: Pointer to a function to call for each node.
  *
  */
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
 	{
 		binary_tree_postorder(tree->left, func);
+		func(tree->n);
 		binary_tree_postorder(tree->right, func);
-		func(tree->n)
 	}
 }
