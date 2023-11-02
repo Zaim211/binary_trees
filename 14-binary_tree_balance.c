0dcd9c2e62522a1b7a4d23c931e16d05c82a6e2e
Zaim211 <yousseftitzaim@gmail.com>
2023-11-02 10:23:28 -0700

    Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,4 @@
-#include "bainary_trees.h"
+#include "binary_trees.h"
 
 /**
  * binary_tree_height - Measures the height of a binary tree.
@@ -34,6 +34,6 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	return (binary_tree_height(tree->left) -
-		binary_tree_height(tree->right));
+	return (binary_tree_height(tree->left)
+		- binary_tree_height(tree->right));
 }
