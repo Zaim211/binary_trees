9d701a9d201ad957b46c8178f03e4bd6695b1870
Zaim211 <yousseftitzaim@gmail.com>
2023-11-02 09:40:31 -0700

    Size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -14,8 +14,8 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	{
 		size_t n_left, n_right;
 
-		n->left = binary_tree_size(tree->left);
-		n->right = binary_tree_size(tree->right);
+		n_left = binary_tree_size(tree->left);
+		n_right = binary_tree_size(tree->right);
 		return (1 + n_left + n_right);
 	}
 }
