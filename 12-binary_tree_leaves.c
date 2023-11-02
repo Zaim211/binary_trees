35b35539ef2a44c9241d2415540fb890b0afa5d5
Zaim211 <yousseftitzaim@gmail.com>
2023-11-02 09:57:16 -0700

    Leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -28,7 +28,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	else
-		return (binary_tree_leaf(tree) +
+		return (binary_tree_is_leaf(tree) +
 			binary_tree_leaves(tree->left) +
 			binary_tree_leaves(tree->right));
 }
