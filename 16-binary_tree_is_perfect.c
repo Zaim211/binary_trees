1b1d532103f3491e48fe84de4c5805c7e94069c7
Zaim211 <yousseftitzaim@gmail.com>
2023-11-02 10:54:01 -0700

    Sibling

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,8 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measures the height of a binary tree.
- *@tree: pointer to the root node of the tree to measure the height.
+ * binary_tree_height - Measures the height of a binary tree.
+ * @tree: Pointer to the root node of the tree to measure the height.
+ *
  * Return: if tree is NULL, your function must return 0.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
@@ -24,8 +25,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 }
 /**
- * binary_tree_size - measures the size of a binary tree.
- *@tree: pointer to the root node of the tree to measure the size.
+ * binary_tree_size - Measures the size of a binary tree.
+ * @tree: Pointer to the root node of the tree to measure the size.
+ *
  * Return: if tree is NULL, must return 0.
  */
 size_t binary_tree_size(const binary_tree_t *tree)
